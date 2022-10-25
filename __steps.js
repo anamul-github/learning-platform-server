874681d8d17c74b1e2779f3be2c66366c389ec1b
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-24 22:27:06 -0400

    installed cors and used it in index.js to fetch data from server

---
+++ __steps.js
@@ -0,0 +1,13 @@
+/* 
+SETUP SERVER:
+1. create a folder in your projects folder
+2. go to that folder with cmd (that you created for your server)
+3. npm init -y (saying yes to all the questions)
+4. open a index.js file
+5 npm i express 
+6. write the boilerplate code inside index.js
+7. nodemon index.js [to see it live] (Since we already installed nodemon long time ago)
+8. make some data in a new json file(inside Data folder)
+9. add git.ignore file and write "/node_modules" 
+10. Push it to github
+*/
